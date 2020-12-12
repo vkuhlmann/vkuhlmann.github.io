fdb6185a0d999a62462b8204cd4512bc65d1c62f
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2020-12-12 17:26:11 +0100

    Improve show more behavior

--- layout.js
+++ layout.js
@@ -6,4 +6,12 @@ $(document).ready(function () {
 
 function onDOMReady() {
     console.log("Hey!");
+
+    for (let projectCardEl of $(".project-card")) {
+        for (let showButton of $(".showProjectDetails", projectCardEl)) {
+            showButton.addEventListener("click", () => {
+                $("#view-projectdetails")[0].innerHTML = $(".project-details", projectCardEl)[0].innerHTML;
+            });
+        }
+    }
 }
