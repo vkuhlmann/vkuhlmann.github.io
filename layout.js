8d2681c6d55c7f57b0bf1a7b067e0870a50bd76c
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2020-12-12 18:50:38 +0100

    Tidy up for first publication

--- layout.js
+++ layout.js
@@ -9,9 +9,18 @@ function onDOMReady() {
 
     for (let projectCardEl of $(".project-card")) {
         for (let showButton of $(".showProjectDetails", projectCardEl)) {
-            showButton.addEventListener("click", () => {
+            showButton.addEventListener("click", (e) => {
                 $("#view-projectdetails")[0].innerHTML = $(".project-details", projectCardEl)[0].innerHTML;
+                $("#view-projectshortdescription")[0].innerHTML = $(".project-shortdescription", projectCardEl)[0].innerHTML;
+                $("#view-projectname")[0].innerHTML = $(".project-name", projectCardEl)[0].innerHTML;
+                $("#view-projectdate")[0].innerHTML = $(".project-date", projectCardEl)[0].innerHTML;
+                $("#view-projectimage")[0].src = $(".project-image", projectCardEl)[0].src;
+
+                e.preventDefault();
+                e.stopPropagation();
             });
         }
     }
+
+    $("#lorentz-interactive .showProjectDetails").trigger("click");
 }
