fe24c625ad57c80e125d12df40a081c088cc302a
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2021-09-09 10:18:24 +0200

    Add VS Code set full path option

--- src/components/VSCodeConfigWizard.js
+++ src/components/VSCodeConfigWizard.js
@@ -13,9 +13,13 @@ import { darkTheme, lightTheme } from "./theme"
 import _ from "lodash";
 
 import texWorkshopDefaults from "../vscodeconfig/workshopDefaults.module.json";
-import {parse as jsoncParse, parseTree as jsoncParseTree, findNodeAtLocation as
-    jsoncFindNodeAtLocation,
-    modify as jsoncModify } from "jsonc-parser";
+// import {parse as jsoncParse, parseTree as jsoncParseTree, findNodeAtLocation as
+//     jsoncFindNodeAtLocation,
+//     modify as jsoncModify,
+//     getNodeValue as jsoncGetNodeValue
+//  } from "jsonc-parser";
+
+import * as jsonc from "jsonc-parser";
 
 // import { Checkbox, Button, Label, Textarea, Box } from '@theme-ui/components';
 import {
@@ -35,34 +39,168 @@ import {
 const SETTING_TOOLS = "latex-workshop.latex.tools";
 const SETTING_RECIPES = "latex-workshop.latex.recipes";
 
-const configSetPdflatexFullpath = (obj, fullPath) => {
-    // let tools = obj[SETTING_TOOLS] ?? texWorkshopDefaults[SETTING_TOOLS];
-    
-    // let res = _.find(tools, tool => tool.name == "pdflatex");
-    // res.command = fullPath;
+const TOOLS_PATH = [SETTING_TOOLS];
 
-    // obj["latex-workshop.latex.tools"] = tools;
-    // return obj;
 
-    let toolsPath = `['${SETTING_TOOLS}']`;
-    toolsPath = [SETTING_TOOLS];
-
-    let rootNode = jsoncParseTree(obj);
-
-    let toolsNode = jsoncFindNodeAtLocation(rootNode, toolsPath);
-    console.log("Toolsnode is");
-    console.log(toolsNode);
-    if (toolsNode == null) {
-        toolsNode = texWorkshopDefaults[SETTING_TOOLS];
-        let edits = jsoncModify(obj, toolsPath, toolsNode, {});
-        console.log("Edits are");
-        console.log(edits);
+let jsoncFormatting = {
+    formattingOptions: {
+        tabSize: 4,
+        insertSpaces: true
     }
-
-    console.log("Object is");
-    console.log(obj);
-
 };
+
+const getDefaultTools = () => {
+    return texWorkshopDefaults[SETTING_TOOLS];
+};
+
+const ensureTools = ({obj}) => {
+    let parseErrors = [];
+    let toolsNode = jsonc.findNodeAtLocation(jsonc.parseTree(obj, parseErrors), TOOLS_PATH);
+    if (toolsNode == null) {
+        toolsNode = getDefaultTools();
+
+        let edits = jsonc.modify(obj, TOOLS_PATH, toolsNode, jsoncFormatting);
+        obj = jsonc.applyEdits(obj, edits);
+    }
+    return {obj, parseErrors};
+};
+
+const ensureTool = (obj, toolName) => {
+    let defaultValue = _.find(getDefaultTools(),
+        tool => tool.name === toolName);
+
+    if (defaultValue == null)
+        throw `Can't ensure tool ${toolName}, no default value provided!`;
+
+    let toolsNode = jsonc.findNodeAtLocation(jsonc.parseTree(obj), TOOLS_PATH);
+    let tools = jsonc.getNodeValue(toolsNode);
+
+    let index = _.findIndex(tools, tool => tool.name === toolName);
+    if (index == -1) {
+        obj = jsonc.applyEdits(obj, jsonc.modify(
+            obj, [...TOOLS_PATH, 0], defaultValue,
+            { ...jsoncFormatting, isArrayInsertion: true }
+        ));
+        return { obj, modified: true };
+    } else {
+        return { obj, modified: false };
+    }
+};
+
+
+const updateTool = ({obj, toolPredicate, toolUpdate}) => {
+    let toolsNode = jsonc.findNodeAtLocation(jsonc.parseTree(obj), TOOLS_PATH);
+    let tools = jsonc.getNodeValue(toolsNode);
+    if (typeof(toolPredicate) == "string" || toolPredicate instanceof String) {
+        let toolName = toolPredicate;
+        toolPredicate = tool => tool.name == toolName;
+        console.log("Converted predicate");
+    }
+    console.log("Tools is");
+    console.log(tools);
+
+    let index = _.findIndex(tools, toolPredicate);
+    console.log(`Update tool found index ${index}`);
+    if (index == -1)
+        return {obj, found: false};
+
+    const {newValue, path} = toolUpdate(tools[index]);
+
+    toolsNode = null;
+    obj = jsonc.applyEdits(obj,
+        jsonc.modify(obj, [...TOOLS_PATH, index, ...path],
+            newValue, {}
+        )
+    );
+    return {obj, found: false};
+};
+
+const updateToolCommand = ({obj, toolPredicate, command}) => {
+    return updateTool({obj, toolPredicate, toolUpdate: origVal => {
+        console.log(`Changing command from ${origVal.command} to ` +
+        `${command}`);
+        return {
+            newValue: command,
+            path: ["command"]
+        };
+    }});
+};
+
+// const configSetPdflatexFullpath = (obj, fullPath, biberPath) => {
+//     // let tools = obj[SETTING_TOOLS] ?? texWorkshopDefaults[SETTING_TOOLS];
+
+//     // let res = _.find(tools, tool => tool.name == "pdflatex");
+//     // res.command = fullPath;
+
+//     // obj["latex-workshop.latex.tools"] = tools;
+//     // return obj;
+
+//     let toolsPath = [SETTING_TOOLS];
+
+//     let errors = [];
+
+//     console.log("Errors is");
+//     console.log(errors);
+
+//     let toolsNode = jsonc.findNodeAtLocation(jsonc.parseTree(obj, errors), toolsPath);
+//     if (toolsNode == null) {
+//         toolsNode = texWorkshopDefaults[SETTING_TOOLS];
+
+//         let edits = jsonc.modify(obj, toolsPath, toolsNode, jsoncFormatting);
+//         obj = jsonc.applyEdits(obj, edits);
+//         toolsNode = jsonc.findNodeAtLocation(jsonc.parseTree(obj, errors), toolsPath);
+//     }
+
+//     const ensureTool = (obj, toolName) => {
+//         let defaultValue = _.find(texWorkshopDefaults[SETTING_TOOLS],
+//             tool => tool.name === toolName);
+
+//         if (defaultValue == null)
+//             throw `Can't ensure tool ${toolName}, no default value provided!`;
+
+//         let toolsNode = jsonc.findNodeAtLocation(jsonc.parseTree(obj, errors),
+//             toolsPath);
+
+//         let tools = jsonc.getNodeValue(toolsNode);
+//         let index = _.findIndex(tools, tool => tool.name === toolName);
+//         if (index == -1) {
+//             obj = jsonc.applyEdits(obj, jsonc.modify(
+//                 obj, [...toolsPath, 0], defaultValue,
+//                 { ...jsoncFormatting, isArrayInsertion: true }
+//             ));
+//             return { obj, modified: true };
+//         } else {
+//             return { obj, modified: false };
+//         }
+//     };
+
+//     const updateToolCommand = (obj, toolName, newValue) => {
+//         let toolsNode = jsonc.findNodeAtLocation(jsonc.parseTree(obj, errors), toolsPath);
+//         let tools = jsonc.getNodeValue(toolsNode);
+//         let index = _.findIndex(tools, tool => tool.name == toolName);
+//         console.log(`Previous ${toolName} cmd: ${tools[index].command}`);
+
+//         toolsNode = null;
+//         obj = jsonc.applyEdits(obj,
+//             jsonc.modify(obj, [...toolsPath, index, "command"],
+//                 newValue, {}
+//             )
+//         );
+//         return obj;
+//     }
+
+//     obj = ensureTool(obj, "pdflatex").obj;
+//     obj = updateToolCommand(obj, "pdflatex", fullPath);
+
+//     if (biberPath != null) {
+//         obj = ensureTool(obj, "biber").obj;
+//         obj = updateToolCommand(obj, "biber", biberPath);
+//     }
+
+//     console.log("Object is");
+//     console.log(jsonc.parseTree(obj, errors));
+//     return obj;
+// };
 
 
 export default (props) => {
@@ -71,9 +209,11 @@ export default (props) => {
     let theme = context.isDarkTheme ? darkTheme : lightTheme;
     const [doPdflatexFull, setDoPdflatexFull] = useState(false);
     const [doPdflatexRecipe, setDoPdflatexRecipe] = useState(false);
+    const [doSetBiberPath, setDoSetBiberPath] = useState(true);
+
     const [statusMsg, setStatusMsg] = useState(false);
 
-    const [pdflatexfullpath, setPdflatexfullpath] = useState("D:\\Programs\\TeX\\pdflatex.exe");
+    const [pdflatexfullpath, setPdflatexfullpath] = useState("");
 
     //let pdflatexfullpath = useRef();
     let currConfig = useRef();
@@ -98,7 +238,7 @@ export default (props) => {
         if (cancel) {
             let target = e.target;
 
-            setTimeout(function() {
+            setTimeout(function () {
                 target.checked = doPdflatexFull;
             }, 0);
 
@@ -139,30 +279,92 @@ export default (props) => {
 
         let obj = val;
 
-        if (doPdflatexFull)
-            obj = configSetPdflatexFullpath(obj, pdflatexfullpath) ?? obj;
-        
+        if (doPdflatexFull) {
+            ({obj} = ensureTools({obj}));
+            let modified = false;
+            ({obj, modified} = ensureTool(obj, "pdflatex"));
+            if (modified) {
+                console.log(`Added default pdflatex`);
+            }
+            ({obj} = updateToolCommand({obj, toolPredicate: "pdflatex", command: pdflatexfullpath}));
+
+            //obj = configSetPdflatexFullpath(obj, pdflatexfullpath, doSetBiberPath ? biberPath : null) ?? obj;
+        }
+
+        if (doPdflatexFull && doSetBiberPath && biberPath != null) {
+            ({obj} = ensureTools({obj}));
+            let modified = false;
+            ({obj, modified} = ensureTool(obj, "biber"));
+            if (modified) {
+                console.log(`Added default biber`);
+            }
+            ({obj} = updateToolCommand({obj, toolPredicate: "biber", command: biberPath}));
+        }
+
+        newConfig.current.value = obj;
         //newConfig.current.value = JSON.stringify(obj, null, 4);
-
-
     };
 
     const generate = () => {
         setStatusMsg();
         try {
             generateUnsafe();
-        } catch(err) {
+        } catch (err) {
             setStatusMsg(`${err}`);
             throw err;
         }
     };
 
+    const currConfigKeyDown = (e) => {
+        // BEGIN Source: https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+        // Modified
+        if (e.key == 'Tab') {
+            e.preventDefault();
+            var start = e.target.selectionStart;
+            var end = e.target.selectionEnd;
+
+            e.target.value = e.target.value.substring(0, start) +
+                "    " + e.target.value.substring(end);
+
+            e.target.selectionStart =
+                e.target.selectionEnd = start + 4;
+        }
+        // END Source
+
+        if (e.key == "Backspace") {
+            let val = e.target.value;
+            if (e.target.selectionStart == e.target.selectionEnd) {
+                let lineStart = val.lastIndexOf("\n", e.target.selectionStart) + 1;
+                let termination = val.substring(lineStart,
+                    e.target.selectionStart);
+                console.log(`Termination is (${termination})`);
+
+                let match = termination.match(/^(?<beforeOnLine>(    |\t)*)(?<removed>\s+)$/);
+                if (match != null) {
+                    let prevPos = e.target.selectionStart;
+                    e.target.value = val.substring(0, lineStart)
+                        + match.groups.beforeOnLine + val.substring(e.target.selectionEnd);
+
+                    e.target.selectionStart = prevPos - match.groups.removed.length;
+                    e.target.selectionEnd = e.target.selectionStart;
+                    e.preventDefault();
+                }
+            }
+        }
+    };
+
+    let biberPath = null;
+    let pdflatexPathMatch = pdflatexfullpath.match(/(?<before>^.*(\/|\\))pdflatex(?<after>(\.exe)?)$/);
+    if (pdflatexPathMatch != null) {
+        biberPath = `${pdflatexPathMatch.groups.before}biber${pdflatexPathMatch.groups.after}`;
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <Box as="form" onSubmit={(e) => e.preventDefault()}>
-                <Label htmlFor="currConfig">Current <code>settings.json</code></Label>
+                <Label htmlFor="currConfig" mb={1}>Current <code>settings.json</code></Label>
                 <Textarea name="currConfig" id="currConfig" rows={6} mb={3}
-                    ref={currConfig} />
+                    ref={currConfig} onKeyDown={currConfigKeyDown} />
                 <Box>
                     <Label mb={3}>
                         <Checkbox onChange={onCheckChange} checked={doPdflatexFull} />
@@ -172,9 +374,27 @@ export default (props) => {
                     </Label>
                     <Box ml={40} sx={pdflatexFullDisabledStyles}>
                         <Label htmlFor="pdflatexfullpath">Full path to pdflatex</Label>
-                        <Input name="pdflatexfullpath" id="pdflatexfullpath" mb={3} 
-                         disabled={!doPdflatexFull} value={pdflatexfullpath}
-                         onChange={e => setPdflatexfullpath(e.target.value)} />
+                        <Input name="pdflatexfullpath" id="pdflatexfullpath" mb={3}
+                            disabled={!doPdflatexFull} value={pdflatexfullpath}
+                            onChange={e => setPdflatexfullpath(e.target.value)} />
+
+                        <Label {...(!biberPath && {
+                            color: "var(--theme-ui-colors-disabledForeground)",
+                        })} mb={3}
+                        >
+                            <Checkbox checked={
+                                biberPath ? doSetBiberPath : false}
+                                disabled={!doPdflatexFull || !biberPath}
+                                onChange={e => setDoSetBiberPath(!doSetBiberPath)} />
+                            <Box>
+                                Set path for biber to{` `}
+                                {biberPath ??
+                                    (<span style={{ color: "red" }}>
+                                        Can't convert path
+                                    </span>)
+                                }
+                            </Box>
+                        </Label>
                     </Box>
                 </Box>
                 <Box>
@@ -186,10 +406,10 @@ export default (props) => {
                     </Label>
                 </Box>
                 {statusMsg && (
-                    <p style={{color: "red"}}>{statusMsg.replace(/\r/g, "").split("\n").map((line, index) => 
-                        (<React.Fragment key={index}>
-                            {line}<br/>
-                        </React.Fragment>)
+                    <p style={{ color: "red" }}>{statusMsg.replace(/\r/g, "").split("\n").map((line, index) =>
+                    (<React.Fragment key={index}>
+                        {line}<br />
+                    </React.Fragment>)
                     )}</p>
                 )}
                 <Button theme={theme} mb={10} onClick={generate}>Generate</Button>
