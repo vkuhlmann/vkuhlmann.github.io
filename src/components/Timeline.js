36cdd3afadde5cd57c20b5b03eab21b998f69997
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2022-03-21 18:15:39 +0100

    Create timeline SVG callout line to HTML

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import clsx from "clsx";
 import _ from "lodash";
 
 function Timeline(props) {
     let events = [
-        {from: 2018, to: 2020, title: "A", track: 0},
-        {from: 2020, to: 2021, title: "B", track: 1}
+        { from: 2018, to: 2020, title: "A", track: 0 },
+        { from: 2020, to: 2021, title: "B", track: 1 }
     ];
 
-    function toFullDate(date, {isUpperBound=false, resolution="day"}={}) {
+    function toFullDate(date, { isUpperBound = false, resolution = "day" } = {}) {
         let m = date.toString().match(/^(?<year>\d{4})(-\d{2})?$/);
         let year = parseInt(m.groups.year);
         let month = parseInt(m.groups.month);
@@ -45,12 +45,12 @@ function Timeline(props) {
     for (let obj of events) {
         obj["from"] = `${obj['from']}`;
         obj["to"] = `${obj['to']}`;
-        
+
         let resolution = "month";
-        obj["fromIncl"] = toFullDate(obj["from"], 
-            {isUpperBound: false, resolution: resolution});
+        obj["fromIncl"] = toFullDate(obj["from"],
+            { isUpperBound: false, resolution: resolution });
         obj["toExcl"] = toFullDate(obj["to"],
-            {isUpperBound: true, resolution: resolution});
+            { isUpperBound: true, resolution: resolution });
     }
 
     console.log(events);
@@ -65,20 +65,20 @@ function Timeline(props) {
     let levelMap =
         _.fromPairs(
             _(dates)
-            .clone()
-            .reverse()
-            .map((val, index, col) => {
-                return [val, index];
-            })
+                .clone()
+                .reverse()
+                .map((val, index, col) => {
+                    return [val, index];
+                })
         );
 
     let trackCount = events.length;
 
-    const trackWidth = 20;
-    const trackSpacing = 10;
-    
+    const trackWidth = 10;
+    const trackSpacing = 3;
+
     function getTrackX(trackIndex) {
-        let left = trackIndex * (trackWidth + trackSpacing);
+        let left = trackIndex * (trackWidth + trackSpacing) + 40;
         let right = left + trackWidth;
         return [left, right];
     }
@@ -95,24 +95,66 @@ function Timeline(props) {
         return [minY, maxY];
     }
 
-    return (
-        <svg viewBox={`${getTrackX(0)[0]} 0 ${getTrackX(trackCount - 1)[1]} 100`}
-            style={{width: "10%"}}>
-            {/* <rectangle x="0" y="0" width="100%" height="100%" fill="#eee" /> */}
-            <rect
-                style={{x: "0", y: "0", width: "100%", height: "100%", fill: "#eee"}}
-            />
-            {
-                _(events).map((val, index) => {
-                    let [minX, maxX] = getTrackX(val.track);
-                    let [minY, maxY] = getEventY(val.fromIncl, val.toExcl);
+    let viewBoxMinX = getTrackX(0)[0];
+    viewBoxMinX = 0;
 
-                    // return <rect key={index} x={0} y={0} width={20} height={60} fill={"red"} />
-                    return <rect key={index} x={minX} y={minY} width={maxX-minX}
-                        height={maxY-minY} fill={"red"} />
-                }).value()
-            }
-        </svg>
+    let target = useRef(null);
+    let connector = useRef(null);
+    let svg = useRef(null);
+
+    useEffect(() => {
+        let targetBoundingRect = target.current.getBoundingClientRect();
+        let targetPos = svg.current.createSVGPoint();
+        targetPos.x = targetBoundingRect.x;
+        targetPos.y = targetBoundingRect.y;
+
+        let gotoPos = targetPos.matrixTransform(svg.current.getScreenCTM().inverse());
+        console.log(gotoPos);
+
+        // connector.current.setAttribute("d", `M 10 10 L 50 20`);
+        connector.current.setAttribute("d", `M 10 10 L ${gotoPos.x} ${gotoPos.y}`);
+
+        // console.log("Current connector:")
+        // console.log(connector.current);
+    });
+
+    return (
+        <>
+            <svg viewBox={`${viewBoxMinX} 0 ${getTrackX(trackCount - 1)[1]} 100`}
+                style={{ width: "10%" }} ref={svg}>
+                {/* <rectangle x="0" y="0" width="100%" height="100%" fill="#eee" /> */}
+                <rect
+                    style={{ x: "0", y: "0", width: "100%", height: "100%", fill: "#eee" }}
+                />
+                {
+                    _(dates).map((val, index) => {
+                        // BEGIN Source: https://stackoverflow.com/questions/4991171/auto-line-wrapping-in-svg-text
+                        // Modified
+                        return (<foreignObject key={index} x="0" y="0" width="150" height="200">
+                            <p xmlns="http://www.w3.org/1999/xhtml">Text goes here</p>
+                        </foreignObject>);
+                        // END Source
+                        return <div style={{ x: 0, y: 0 }}>{getLevelHeight(levelMap[val])}</div>
+                    }).value()
+                }
+                {
+                    _(events).map((val, index) => {
+                        let [minX, maxX] = getTrackX(val.track);
+                        let [minY, maxY] = getEventY(val.fromIncl, val.toExcl);
+
+                        // return <rect key={index} x={0} y={0} width={20} height={60} fill={"red"} />
+                        return <rect key={index} x={minX} y={minY} width={maxX - minX}
+                            height={maxY - minY} fill={"red"} />
+                    }).value()
+                }
+                <path d="M 10 10 L 30 10" stroke="orange" style={{strokeWidth: 3}} ref={connector} />
+                <circle cx="10" cy="10" r={3} fill="blue" />
+            </svg>
+
+            <div>
+                <span ref={target}>Iets van text</span>
+            </div>            
+        </>
     );
 }
 
